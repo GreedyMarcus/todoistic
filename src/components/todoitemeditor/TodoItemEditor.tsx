8a2723bec0cf87802b5403156cb1f8a6366f2480
Mark Domahidi <mdomahidi@gmail.com>
2019-10-15 07:44:55 +0200

    Added basic frame to TodoItemEditor component

--- src/components/todoitemeditor/TodoItemEditor.tsx
+++ src/components/todoitemeditor/TodoItemEditor.tsx
@@ -1,9 +1,14 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import './TodoItemEditor.css';
 
 export class TodoItemEditor extends Component {
   render() {
     return (
-      <div>
+      <div className="TodoItemEditor">
+        <Link className="TodoItemEditor-link" to="/">
+          <span className="TodoItemEditor-close">&times;</span>
+        </Link>
         <h2>Todo Item Editor!</h2>
       </div>
     );
