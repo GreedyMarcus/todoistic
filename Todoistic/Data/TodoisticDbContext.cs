e6a05a7c1a8ae5967ad0765fdf390800589cb1ab
Mark Domahidi <mdomahidi@gmail.com>
2019-11-15 00:09:58 +0100

    Implemented data layer

---
+++ Todoistic/Data/TodoisticDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Todoistic.Models;
+
+namespace Todoistic.Data
+{
+    public class TodoisticDbContext : DbContext
+    {
+        public TodoisticDbContext(DbContextOptions<TodoisticDbContext> options) : base(options) { }
+
+        public DbSet<TodoItem> TodoItems { get; set; }
+    }
+}
---
+++ Todoistic/Models/TodoItem.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Todoistic.Models
+{
+    public enum Status
+    {
+        Todo = 0,
+        InProgress = 1,
+        Done = 2,
+        Postponed = 3
+    }
+
+    public class TodoItem
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime Due { get; set; }
+        public Status Status { get; set; }
+        public int Priority { get; set; }
+        public int PriorityMax { get; set; }
+    }
+}
